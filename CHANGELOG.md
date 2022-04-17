a39ca7e155a2c7024f88f56cde117cd807cf7704
Brian Smith <ohohvi@gmail.com>
2022-04-17 12:11:30 -0700

    Add --help and --version subcommands

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 ## 0.1.2 (2022-04-17)
 
 - More targeted AWS regex
+- Add --help and --version subcommands
 
 ## 0.1.1 (2022-04-16)
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::cmp;
 use std::env;
 use std::error::Error;
+use std::fmt;
 use std::process;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
@@ -122,40 +123,80 @@ fn find_secrets(path: &str, additional_paths: &[&str]) -> Result<usize, Box<dyn 
     Ok(match_count.fetch_max(0, Ordering::SeqCst))
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let args: Vec<&str> = args.iter().map(|x| &**x).collect();
+#[derive(Debug)]
+pub enum UsageError {
+    PreCommit,
+    Version,
+    Help,
+}
+
+impl std::error::Error for UsageError {}
+
+impl fmt::Display for UsageError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            UsageError::PreCommit => write!(f, "Usage: secrets --install-pre-commit"),
+            UsageError::Version => write!(f, "Usage: secrets --version"),
+            UsageError::Help => write!(f, "Usage: secrets --help"),
+        }
+    }
+}
+
+fn main_aux(args: &Vec<&str>) -> Result<usize, Box<dyn Error>> {
+    let mut match_count: usize = 0;
     let mut path = ".";
 
     if args.len() > 1 && args[1] == "--install-pre-commit" {
         if args.len() > 2 {
-            eprintln!("Usage: secrets --install-pre-commit");
-            process::exit(1);
+            return Err(Box::new(UsageError::PreCommit));
         }
-        match pre_commit::install_pre_commit(path) {
-            Err(err) => {
-                eprintln!("{}", err);
-                process::exit(2);
-            }
-            Ok(()) => {}
+        pre_commit::install_pre_commit(path)?;
+    } else if args.len() > 1 && args[1] == "--version" {
+        if args.len() > 2 {
+            return Err(Box::new(UsageError::Version));
         }
+        println!("secrets {}", env!("CARGO_PKG_VERSION"));
+    } else if args.len() > 1 && args[1] == "--help" {
+        if args.len() > 2 {
+            return Err(Box::new(UsageError::Help));
+        }
+        println!("secrets {}
+
+secrets searches files and directories recursively for secret API keys.
+It's primarily designed to be used as a pre-commit to prevent committing
+secrets into version control.
+
+USAGE:
+    secrets [PATH ...]
+    secrets --install-pre-commit
+    secrets --help
+    secrets --version", env!("CARGO_PKG_VERSION"))
     } else {
         let mut additional_paths: &[&str] = &[];
         if args.len() > 1 {
             path = &args[1];
             if args.len() > 2 {
-                additional_paths = &args[2..]
+                additional_paths = &args[2..];
             }
         }
-        match find_secrets(path, &additional_paths) {
-            Err(err) => {
-                eprintln!("{}", err);
-                process::exit(2);
-            }
-            Ok(match_count) => {
-                if match_count > 0 {
-                    process::exit(1);
-                }
+        match_count = find_secrets(path, &additional_paths)?;
+    }
+
+    return Ok(match_count);
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let args: Vec<&str> = args.iter().map(|x| &**x).collect();
+
+    match main_aux(&args) {
+        Err(err) => {
+            eprintln!("{}", err);
+            process::exit(2);
+        }
+        Ok(match_count) => {
+            if match_count > 0 {
+                process::exit(1);
             }
         }
     }
