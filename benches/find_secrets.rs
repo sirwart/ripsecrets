89c998b55643d5c8505b9fd92a04c63e9d916075
Sam Schlinkert <sschlinkert@gmail.com>
2022-04-23 14:24:51 -0400

    adds parameters to criterion benchmark to make it a bit more thorough, at the cost of run-time

--- benches/find_secrets.rs
+++ benches/find_secrets.rs
@@ -1,10 +1,16 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use ripsecrets::find_secrets;
 use std::path::PathBuf;
+use std::time::Duration;
 
 fn criterion_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Find secrets in self");
+    group
+        .sample_size(150)
+        .measurement_time(Duration::new(15, 0));
     let paths = &[PathBuf::from(".")];
-    c.bench_function("find secrets self", |b| {
+
+    group.bench_function("find_secrets function", |b| {
         b.iter(|| find_secrets(black_box(paths), false, false))
     });
 }
