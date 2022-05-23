4565be1562b9232955171ee93b0648deda234558
Sam Schlinkert <sschlinkert@gmail.com>
2022-05-23 00:59:19 -0400

    re-works src/main.rs to better handle errors. (#36)
    * uses an enum to make the 4 possible results of a run of the program more clear

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use clap::Parser;
-use std::fmt;
 use std::path::PathBuf;
 use std::process;
 
@@ -14,18 +13,6 @@ pub enum UsageError {
     PreCommit,
     Version,
     Help,
-}
-
-impl std::error::Error for UsageError {}
-
-impl fmt::Display for UsageError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            UsageError::PreCommit => write!(f, "Usage: ripsecrets --install-pre-commit"),
-            UsageError::Version => write!(f, "Usage: ripsecrets --version"),
-            UsageError::Help => write!(f, "Usage: ripsecrets --help"),
-        }
-    }
 }
 
 /// Prevent committing secret keys into your source code
@@ -62,7 +49,26 @@ struct Args {
     paths: Vec<PathBuf>,
 }
 
+enum RunResult {
+    PreCommitInstallSuccessful,
+    NoSecretsFound,
+    SecretsFound,
+    Error(String),
+}
+
 fn main() {
+    match run() {
+        RunResult::PreCommitInstallSuccessful => process::exit(0),
+        RunResult::NoSecretsFound => process::exit(0),
+        RunResult::SecretsFound => process::exit(1),
+        RunResult::Error(e) => {
+            eprintln!("Error: {}", e);
+            process::exit(2)
+        }
+    }
+}
+
+fn run() -> RunResult {
     let args = Args::parse();
     let paths = if args.paths.is_empty() {
         vec![PathBuf::from(".")]
@@ -71,31 +77,41 @@ fn main() {
     };
 
     if args.install_pre_commit && (args.strict_ignore || args.only_matching) {
-        let option = if args.strict_ignore { "--strict-ignore" } else { "--only-matching" };
-        eprintln!("Error: {} is not a valid option when installing pre-commits. Use --install-pre-commit alone", option);
-        process::exit(2);
+        let option = if args.strict_ignore {
+            "--strict-ignore"
+        } else {
+            "--only-matching"
+        };
+        return RunResult::Error(format!(
+            "{} is not a valid option when installing pre-commits. Use --install-pre-commit alone",
+            option
+        ));
     }
 
     if args.install_pre_commit {
         for path in paths {
             match pre_commit::install_pre_commit(&path) {
+                // If we installed pre-commit to this path
+                // just fine, keep iterating
                 Ok(()) => (),
                 Err(err) => {
-                    eprintln!("{}", err);
-                    process::exit(2);
+                    // If we get an error when trying to install
+                    // pre-commit, return an Error as a String
+                    return RunResult::Error(err.to_string());
                 }
             }
         }
+        // Made it through all the paths just fine
+        RunResult::PreCommitInstallSuccessful
     } else {
         match find_secrets::find_secrets(&paths, args.strict_ignore, args.only_matching) {
-            Ok(0) => process::exit(0),
-            // We already printed info on discovered secrets,
-            // so just exit
-            Ok(_num_secrets) => process::exit(1),
-            Err(err) => {
-                eprintln!("{}", err);
-                process::exit(2);
-            }
-        };
+            Ok(0) => RunResult::NoSecretsFound,
+            // If we found 1 or more secrets, it's not an error, BUT we do
+            // want to notify the user via a different exit code.
+            Ok(_num_secrets) => RunResult::SecretsFound,
+            // If there's a real error, return it as a String for main()
+            // to handle.
+            Err(err) => RunResult::Error(err.to_string()),
+        }
     }
 }
