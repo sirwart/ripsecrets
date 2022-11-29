30a3c80f4ecde56780e6e50216332601979d3775
Joseph M LaFreniere <joseph@lafreniere.xyz>
2022-11-28 22:33:50 -0600

    Add pre-commit checks (#45)
    * Configure pre-commit hooks as part of Nix shell
    
    The pre-commit hooks added are
    - nixfmt
    - rustfmt
    - cargo check
    - clippy
    
    * Autoformat Nix files using `nixfmt`
    
    * Run pre-commit via Nix as part of CI workflow
    
    * Gitignore `/result`
    
    * Autoformat Rust source files using `rustfmt`
    
    * Skip build as part of `nix flake check`

--- flake.nix
+++ flake.nix
@@ -2,34 +2,52 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     naersk.url = "github:nix-community/naersk";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils, naersk }:
+  outputs = { self, nixpkgs, flake-utils, naersk, pre-commit-hooks }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages."${system}";
         naersk-lib = naersk.lib."${system}";
-      in
-        rec {
-          # `nix build`
-          packages.ripsecrets = naersk-lib.buildPackage {
-            pname = "ripsecrets";
-            root = ./.;
-            doCheck = true;
-            checkInputs = [ pkgs.nix ];
-          };
-          defaultPackage = packages.ripsecrets;
+        pre-commit = pre-commit-hooks.lib."${system}".run;
+      in rec {
+        # `nix build`
+        packages.ripsecrets = naersk-lib.buildPackage {
+          pname = "ripsecrets";
+          root = ./.;
+          doCheck = true;
+          checkInputs = [ pkgs.nix ];
+        };
+        defaultPackage = packages.ripsecrets;
 
-          # `nix run`
-          apps.ripsecrets = flake-utils.lib.mkApp {
-            drv = packages.ripsecrets;
-          };
-          defaultApp = apps.ripsecrets;
+        # `nix run`
+        apps.ripsecrets = flake-utils.lib.mkApp { drv = packages.ripsecrets; };
+        defaultApp = apps.ripsecrets;
 
-          # `nix develop`
-          devShell = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [ cargo gnuplot libiconv rustc ];
+        checks = {
+          build = self.defaultPackage.${system};
+          pre-commit-check = pre-commit {
+            src = ./.;
+            hooks = {
+              nixfmt.enable = true;
+              rustfmt.enable = true;
+              cargo-check.enable = true;
+            };
           };
-        }
-    );
+        };
+
+        # `nix develop`
+        devShell = pkgs.mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+          nativeBuildInputs = with pkgs; [
+            cargo
+            gcc
+            libiconv
+            nixfmt
+            rustc
+            rustfmt
+          ];
+        };
+      });
 }
