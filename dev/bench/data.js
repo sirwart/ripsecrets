4f77ef6bbe97ac18edb21b2a0226e7b8983e2627
github-action-benchmark <github@users.noreply.github.com>
2023-12-11 18:38:16 +0000

    add Benchmark (cargo) benchmark result for 8a7627cfc4412203d059ee43df320696f10d1daf

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1694449570626,
+  "lastUpdate": 1702319896296,
   "repoUrl": "https://github.com/sirwart/ripsecrets",
   "entries": {
     "Benchmark": [
@@ -539,6 +539,36 @@ window.BENCHMARK_DATA = {
             "name": "Find secrets in getsentry/sentry/find_secrets function",
             "value": 712993930,
             "range": "± 8400661",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "git@lafreniere.xyz",
+            "name": "Joseph M LaFreniere",
+            "username": "lafrenierejm"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8a7627cfc4412203d059ee43df320696f10d1daf",
+          "message": "Update deps (#80)\n\nBumps rustc to 1.73.0 (cc66ad468 2023-10-03)",
+          "timestamp": "2023-12-11T10:33:45-08:00",
+          "tree_id": "4276ce150286614fb6c20fa5e3f269f2487b883d",
+          "url": "https://github.com/sirwart/ripsecrets/commit/8a7627cfc4412203d059ee43df320696f10d1daf"
+        },
+        "date": 1702319895265,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "Find secrets in getsentry/sentry/find_secrets function",
+            "value": 431411248,
+            "range": "± 1297125",
             "unit": "ns/iter"
           }
         ]
