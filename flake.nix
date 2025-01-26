9b43cd14a887b1727a8f13d03d99e4b3833f04f9
Joseph LaFreniere <git@lafreniere.xyz>
2025-01-25 19:19:40 -0500

    Update Flake inputs to account for Cargo v4 (#92)
    Also remove unnecessary `write` call found by Clippy

--- flake.nix
+++ flake.nix
@@ -4,24 +4,16 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
-
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
+    crane.url = "github:ipetkov/crane";
     flake-utils.url = "github:numtide/flake-utils";
-
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     advisory-db = {
       url = "github:rustsec/advisory-db";
       flake = false;
     };
-
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
 
